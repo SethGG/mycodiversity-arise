af173774f51f071f64297ca48d8ff06d4e57529f
dzee <df.zee@hotmail.com>
2024-11-10 15:57:37 +0100

    df approach

--- createTruncateTable.py
+++ createTruncateTable.py
@@ -1,70 +1,46 @@
 import os
 import sys
 import re
-import csv
-from collections import defaultdict
+import pandas as pd
 
 
 def main(input_dir, output_dir):
-    pattern = re.compile(r"^(NBCLAB\d+?)_zotus_trunc_mapping.txt$")
-    with open(os.path.join(output_dir, "truncate_mapping.csv"), "w") as tm_out, \
-            open(os.path.join(output_dir, "filter_mapping_full.csv"), "w") as fmf_out, \
-            open(os.path.join(output_dir, "filter_mapping_trunc.csv"), "w") as fmt_out:
+    pattern_zotutab = re.compile(r"^(NBCLAB\d+?)_zotutab.txt$")
+    df_trunc_mapping = pd.DataFrame(columns=['srr_name', 'zotu_id', 'trunc_mapping'])
 
-        csv_writer_tm = csv.writer(tm_out)
-        csv_writer_tm.writerow(['srr_name', 'zotu_id', 'trunc_zotu_id'])
+    df_filter_mapping_trunc = pd.DataFrame(columns=['srr_name', 'zotu_id', 'passed_abun', 'passed_contam'])
+    df_filter_mapping_full = pd.DataFrame(columns=['srr_name', 'zotu_id', 'passed_abun', 'passed_contam'])
+    df_filter_mapping = {"": df_filter_mapping_trunc, "_full": df_filter_mapping_full}
 
-        csv_writer_fmf = csv.writer(fmf_out)
-        csv_writer_fmf.writerow(['srr_name', 'zotu_id', 'passed_abun', 'passed_contam'])
-
-        csv_writer_fmt = csv.writer(fmt_out)
-        csv_writer_fmt.writerow(['srr_name', 'zotu_id', 'passed_abun', 'passed_contam'])
-
-        srr_names = []
-
-        for filename in sorted(os.listdir(f"{input_dir}/ZOTUS")):
-            input_path = os.path.join(input_dir, "ZOTUS", filename)
-            match = pattern.match(filename)
+    for suffix in df_filter_mapping:
+        zotus_dir = os.path.join(input_dir, f"ZOTUS{suffix}")
+        for filename in sorted(os.listdir(zotus_dir)):
+            match = pattern_zotutab.match(filename)
             if match:
                 srr_name = match.groups()[0]
-                srr_names.append(srr_name)
 
-                with open(input_path, "r") as f:
-                    for line in f:
-                        parts = line.strip().split()
-                        zotu, cluster = parts[0], parts[1]
-                        csv_writer_tm.writerow([srr_name, zotu, cluster])
+                if suffix == "":
+                    trunc_mapping_path = os.path.join(zotus_dir, f"{srr_name}_zotus_trunc_mapping.txt")
+                    df = pd.read_csv(trunc_mapping_path, sep="\t", usecols=[0, 1], names=["zotu_id", "trunc_mapping"])
+                    df['srr_name'] = srr_name
+                    df_trunc_mapping = pd.concat([df_trunc_mapping, df])
 
-        suffix = {"": csv_writer_fmt, "_full": csv_writer_fmf}
-        for srr_name in srr_names:
-            passed_abun = {s: defaultdict(bool) for s in suffix}
-            passed_contam = {s: defaultdict(bool) for s in suffix}
-            for s in suffix:
-                abun_path = f"ZOTUS{s}/abundant/{srr_name}_zotutab_af.txt"
-                contam_path = f"FINAL{s}/{srr_name}_zotutab_final.txt"
-                abun = (abun_path, passed_abun)
-                contam = (contam_path, passed_contam)
-                for filter in (abun, contam):
-                    header = None
-                    with open(f"{input_dir}/{filter[0]}", "r") as f:
-                        for line in f:
-                            if header is None:
-                                header = line
-                            else:
-                                parts = line.strip().split()
-                                zotu = parts[0]
-                                filter[1][s][zotu] = True
+                zotutab_path = os.path.join(zotus_dir, filename)
+                df_zotutab = pd.read_csv(zotutab_path, sep="\t", usecols=[0], names=["zotu_id"], header=0)
 
-                zotu_path = f"ZOTUS{s}/{srr_name}_zotutab.txt"
-                header = None
-                with open(f"{input_dir}/{zotu_path}", "r") as f:
-                    for line in f:
-                        if header is None:
-                            header = line
-                        else:
-                            parts = line.strip().split()
-                            zotu = parts[0]
-                            suffix[s].writerow([srr_name, zotu, passed_abun[s][zotu], passed_contam[s][zotu]])
+                abun_path = os.path.join(zotus_dir, 'abundant', f"{srr_name}_zotutab_af.txt")
+                contam_path = os.path.join(input_dir, f"FINAL{suffix}", f"{srr_name}_zotutab_final.txt")
+                df_abun = pd.read_csv(abun_path, sep="\t", usecols=[0], names=["zotu_id_abun"], header=0)
+                df_contam = pd.read_csv(contam_path, sep="\t", usecols=[0], names=["zotu_id_contam"], header=0)
+
+                df_merged_abun = df_zotutab.merge(df_abun, how="left", left_on="zotu_id", right_on="zotu_id_abun")
+                df_merged_contam = df_zotutab.merge(df_contam, how="left", left_on="zotu_id", right_on="zotu_id_contam")
+                df_zotutab['passed_abun'] = df_merged_abun["zotu_id_abun"].notna()
+                df_zotutab['passed_contam'] = df_merged_contam["zotu_id_contam"].notna()
+
+                print(df_zotutab)
+
+    df_trunc_mapping.to_csv(os.path.join(output_dir, "truncate_mapping.csv"))
 
 
 if __name__ == "__main__":
